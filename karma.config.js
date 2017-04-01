4a6fd784c2e992ef32d1dff4317278b331d9e492
ssiraj <SHAKIL.SIRAJ@GMAIL.COM>
2017-04-01 05:24:21 +0000

    Fixed karma testing

--- karma.config.js
+++ karma.config.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: './spec',
 
 
     // frameworks to use
@@ -16,11 +16,16 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       {
-        pattern: "./spec/unit/**/*.spec.ts"
+        pattern: "./support/karma.setup.ts"
+        },
+      {
+        pattern: "./unit/**/*.+(ts|html|css)"
       }
     ],
 
-
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
     // list of files to exclude
     exclude: [
     ],
@@ -32,12 +37,24 @@ module.exports = function(config) {
       "**/*.ts": ["karma-typescript"]
     },
 
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+        transforms: [
+                    require("karma-typescript-angular2-transform")
+                ]
+      },
+      tsconfig: "../tsconfig.karma.json",
+      coverageOptions: {
+        instrumentation: true
+      }
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    //reporters: ['progress'],
-    reporters: ['spec'],
+    // reporters: ['progress'],
+    reporters: ['spec', 'kjhtml'],
     specReporter: {
       maxLogLines: 5, // limit number of lines logged per test 
       suppressErrorSummary: false, // do not print error summary 
@@ -58,7 +75,6 @@ module.exports = function(config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
@@ -70,15 +86,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+    // browsers: [],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+    //Enabling this setting will cancel the current run and start a new run immediately when a change is detected.
+    restartOnFileChange: true
   })
 }
--- spec/unit/test.spec.ts
+++ spec/unit/test.spec.ts
@@ -1,3 +1,7 @@
+import { Component, ViewEncapsulation, DebugElement} from '@angular/core';
+import { ComponentFixture, TestBed, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 describe("Tesing project setup", ()=>{
     it("Tesing that unit test setup is running", ()=>{
         let toBeTrue: boolean = true;
@@ -16,4 +20,29 @@ describe("Tesing project setup", ()=>{
         expect(instance.random > 0).toBeTruthy();
         
     });
+});
+
+describe("Testing Angular features", ()=>{
+    it("Testing components", ()=>{
+        
+        @Component({
+            selector: 'container-test',
+            template: '<div>Hello Testing</div>',
+            encapsulation: ViewEncapsulation.None
+        })
+        class ContainerTestComponent {}
+        
+
+        TestBed.configureTestingModule({
+            declarations: [ContainerTestComponent]
+        });
+        
+        let fixture: ComponentFixture<ContainerTestComponent> = TestBed.createComponent(ContainerTestComponent);
+        let debugElement: DebugElement = fixture.debugElement;
+        let nativeElement : HTMLElement = debugElement.nativeElement;
+        expect(nativeElement.innerHTML).toBe("<div>Hello Testing</div>");
+        
+    });
+    
+    
 });
